e00a23ec3ceb97264ba43aeac3e29540ba0c12d1
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-07-11 15:12:00 +0200

    dodanie sprawdzania waluty przy dodaniu nowego rachunku w accountService

--- src/main/java/com/example/demo/services/AccountServiceImpl.java
+++ src/main/java/com/example/demo/services/AccountServiceImpl.java
@@ -1,10 +1,17 @@
 package com.example.demo.services;
 
+import com.example.demo.DTOs.CurrencyDto;
 import com.example.demo.entities.Account;
 import com.example.demo.exceptions.AccountDoesNotExistException;
+import com.example.demo.exceptions.ConnectionException;
+import com.example.demo.exceptions.CurrencyIsNotAvailableException;
 import com.example.demo.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 
 @Service
@@ -19,6 +26,22 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void addAccount(Account account) {
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "https://api.exchangeratesapi.io/latest?base=" + account.getCurrency();
+            ResponseEntity<CurrencyDto> response = restTemplate.getForEntity(url, CurrencyDto.class);
+            CurrencyDto currencyDto = response.getBody();
+
+            currencyDto.getRates().get(account.getCurrency());
+        }
+        catch (NullPointerException ex) {
+            throw new CurrencyIsNotAvailableException("Blad w konwersji walut");
+        }
+        catch (ResourceAccessException ex) {
+            throw new ConnectionException("Blad w polaczeniu z API");
+        }
+
         accountRepository.save(account);
     }
 
@@ -51,7 +74,7 @@ public class AccountServiceImpl implements AccountService {
             }
 
             addAccount(accountToSave);
-            accountRepository.save(accountToSave);
+            //accountRepository.save(accountToSave);
         }
         else {
             throw new AccountDoesNotExistException("Rachunek nie istnieje!");
