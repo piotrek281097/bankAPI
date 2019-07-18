afb6cfbfa6e5f39e1b30617056b7f7df57d66ff1
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-07-18 14:18:34 +0200

    usuniecie komentarza

--- src/main/java/com/example/demo/services/AccountServiceImpl.java
+++ src/main/java/com/example/demo/services/AccountServiceImpl.java
@@ -83,7 +83,6 @@ public class AccountServiceImpl implements AccountService {
                 accountToSave.setOwnerName(account.getOwnerName());
             }
 
-            //addAccount(accountToSave);
             accountRepository.save(accountToSave);
         }
         else {
