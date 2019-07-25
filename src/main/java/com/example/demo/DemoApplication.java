596e321d1855bc424573dc4e80b5a23dcd64eccd
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-07-25 13:21:28 +0200

    poprawka18 do wyslania maila, wywalenie beana

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,22 +18,6 @@ public class DemoApplication extends SpringBootServletInitializer {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
 
-		mailSender.setUsername("piotrbankapi2");
-		mailSender.setPassword("Piotrek2810$");
-
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-
-		return mailSender;
-	}
 
 }
